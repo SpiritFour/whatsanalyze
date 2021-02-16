e98fab53401b9c616ca683a3debd652128448dd0
Moritz Wolf <code@moritzwolf.com>
2021-02-16 15:28:30 +0100

    add chart.js

---
+++ components/BarChart.vue
@@ -0,0 +1,15 @@
+<script>
+import { Bar } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+  mounted() {
+    this.renderChart([10, 20, 30, 40, 50, 60, 70], {
+      barPercentage: 0.5,
+      barThickness: 6
+    });
+  }
+};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,48 @@
 <template>
   <v-content>
     <FileHandler />
+    <BarChart />
   </v-content>
 </template>
 
 <script>
-// /pages/payment-page.vue
 import DataFrame from "dataframe-js";
 export default {
-  data () {
+  data() {
     return {
       isStripeLoaded: false
+    };
+  },
+  methods: {
+    test() {
+      const df = new DataFrame(
+        [
+          { c1: 1, c2: 6 },
+          { c4: 1, c3: 2 }
+        ],
+        ["c1", "c2", "c3", "c4"]
+      );
+      console.log(df);
     }
   },
-  methods: {test(){
-      const df = new DataFrame([
-        {c1: 1, c2: 6},
-        {c4: 1, c3: 2}
-      ], ['c1', 'c2', 'c3', 'c4']);
-      console.log(df)
-    },},
-  head () {
+  head() {
     return {
-      title: 'Payment Page - My awesome project',
+      title: "Payment Page - My awesome project",
       script: [
         {
-          hid: 'stripe',
-          src: 'https://gmousse.github.io/dataframe-js/dist/dataframe.js',
+          hid: "stripe",
+          src: "https://gmousse.github.io/dataframe-js/dist/dataframe.js",
           defer: true,
           // Changed after script load
-          callback: () => { this.isStripeLoaded = true; this.test() }
+          callback: () => {
+            this.isStripeLoaded = true;
+            this.test();
+          }
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <style></style>
