baafdb26854d39f7a40bdb8038b05d7e6bdad92e
Paul Kehnel <ga65coy@mytum.de>
2021-02-17 14:48:28 +0100

    updated data structure, removed dataframe-js

--- components/FileHandler.vue
+++ components/FileHandler.vue
@@ -41,6 +41,17 @@ export default {
     },
   },
   methods: {
+    extendDataStructure(messages) {
+      let authors = {};
+      messages.forEach(function (object, index) {
+        if (!(object.author in authors)) authors[object.author] = 0;
+        else authors[object.author] += 1;
+        object.absolute_counter = index;
+        object.personal_counter = authors[object.author];
+      });
+      return messages;
+    },
+
     dragOver() {
       this.isDragging = true;
     },
@@ -67,9 +78,9 @@ export default {
           reader.onload = (f) => {
             this.textSource = f.target.result;
             this.isDragging = false;
-            parseString(this.textSource).then(
-              (messages) => (this.messages = messages)
-            );
+            parseString(this.textSource)
+              .then(this.extendDataStructure)
+              .then((messages) => (this.messages = messages));
           };
           // this is the method to read a text file content
           reader.readAsText(file);
