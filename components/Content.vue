d29c6747246ff468031f4079dc5af3df43c87795
Moritz Wolf <32100482+mowolf@users.noreply.github.com>
2021-02-18 15:26:28 +0100

    Revert "better frontpage"

--- components/Content.vue
+++ components/Content.vue
@@ -1,17 +1,7 @@
 <template>
-  <article class="article pt-4 pb-4">
-    <h1 style="font-size: 3em; margin-bottom: 1em">{{ page.title }}</h1>
+  <article style="padding-top: 1em; padding-bottom: 2em">
+    <h1>{{ page.title }}</h1>
     <nuxt-content :document="page" />
-
-    <div>
-      <ol>
-        <li v-for="heading in page.toc" :key="heading">
-          <a :href="'#' + heading.id">
-            {{ heading.text }}
-          </a>
-        </li>
-      </ol>
-    </div>
   </article>
 </template>
 
@@ -21,24 +11,8 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.article {
-  background: $c-white;
-  position: relative;
-  padding: 3em;
-}
+<style scoped>
 .nuxt-content h1 {
-  font-size: 4em;
-  margin-bottom: 1em;
-}
-.nuxt-content h2 {
-  font-size: 2em;
-  margin-top: 0.7em;
-  margin-bottom: 0.7em;
-}
-.nuxt-content h3 {
-  font-size: 1.5em;
-  margin-bottom: 0.7em;
-  margin-top: 0.7em;
+  /* my custom h1 style */
 }
 </style>
