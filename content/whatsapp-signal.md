e30fe31e28ed09e2a68acea1a9a4aa1ede4f9bce
Moritz Wolf <code@moritzwolf.com>
2021-02-18 17:33:04 +0100

    setup basic page structure

---
+++ content/whatsapp-signal.md
@@ -0,0 +1,6 @@
+---
+title: Switch from WhatsApp to Signal
+description: Switch from WhatsApp to Signal
+---
+
+# Switch from WhatsApp to Signal
---
+++ pages/whatsapp-signal.vue
@@ -0,0 +1,14 @@
+<template>
+  <Content :page="page" class="main-el mt-15" />
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const page = await $content("whatsapp-signal").fetch();
+    return {
+      page,
+    };
+  },
+};
+</script>
