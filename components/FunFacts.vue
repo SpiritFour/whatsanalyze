d15856961beb812799ef70d5e9a9ac830c2b4b12
Moritz Wolf <code@moritzwolf.com>
2021-02-19 10:11:33 +0100

    add component

---
+++ components/FunFacts.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- number of words -->
+
+    <!-- emojies ranked by frequency -->
+
+    <!-- longest message -->
+
+    <!-- number of unique words -->
+
+    <!-- average words per message -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "numWords",
+    "emojiesByFreq",
+    "longestMessage",
+    "numUniqueWords",
+    "averageWordsbyFreq",
+  ],
+};
+</script>
+
+<style></style>
