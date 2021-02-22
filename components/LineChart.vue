1bb04a7b270b271b086781f2a1f2c968cf7dcc81
Sebastian Fellner <fellnerseb@gmail.com>
2021-02-22 10:55:10 +0100

    add generated css files to git ignaore

--- components/LineChart.vue
+++ components/LineChart.vue
@@ -24,6 +24,11 @@ export default {
               },
             ],
           },
+          elements: {
+            line: {
+              tension: 0,
+            },
+          },
         };
       },
     },
