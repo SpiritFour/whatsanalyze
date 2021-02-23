343ecd4047f748907a98518b064d0b0e70e3c296
adrianthiesen <adrian.thiesen@gmail.com>
2021-02-23 11:13:11 +0100

    added old cta again

---
+++ components/CtaBottom.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="cta-bottom">
+    <h1 style="text-align: center">Analyse your own whatsapp data now</h1>
+    <span>
+      Interested about your own chat data? Reveal some interesting facts now.
+      Start with figuring out how many toilet rolls would be needed to print
+      your whole chat. Take deep dive in your data now!.
+    </span>
+    <br />
+    <v-btn to="#drop" color="#07bc4c" style="color: #ffffff" class="my-5">
+      Analyse my chat
+    </v-btn>
+  </div>
+</template>
