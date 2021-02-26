755144ff3c78d774cd8debdd15a583d8503c2cb2
Sebastian Fellner <fellnerseb@gmail.com>
2021-02-26 17:14:22 +0100

    smaller icons on carousel

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,28 +37,6 @@
           style="vertical-align: sub"
         />
         WhatsAnalyze
-        <form
-          action="https://www.paypal.com/donate"
-          method="post"
-          target="_top"
-        >
-          <input type="hidden" name="hosted_button_id" value="EPCYG8WEF289G" />
-          <input
-            type="image"
-            src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-            border="0"
-            name="submit"
-            title="PayPal - The safer, easier way to pay online!"
-            alt="Donate with PayPal button"
-          />
-          <img
-            alt=""
-            border="0"
-            src="https://www.paypal.com/en_US/i/scr/pixel.gif"
-            width="1"
-            height="1"
-          />
-        </form>
       </h3>
 
       <h3 class="mt-5">&copy; {{ new Date().getFullYear() }}</h3>
