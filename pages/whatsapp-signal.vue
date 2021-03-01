20de0b9e382691a4b1836c68e4e46755c0ec0c7d
adrianthiesen <adrian.thiesen@gmail.com>
2021-03-01 13:50:12 +0100

    adjust meta tags for signal page

--- pages/whatsapp-signal.vue
+++ pages/whatsapp-signal.vue
@@ -11,5 +11,19 @@ export default {
       page,
     };
   },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Learn how to export your chat from WhatsApp to Signal ✓ Now working for Group Chats ✓ Keep your Data ✓ No Chat Data is sent to a Server. Get Started now!",
+        },
+      ],
+    };
+  },
 };
 </script>
