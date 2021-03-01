2d01a7bc0b58e8ac46a5cdbe3412f1cd44696b15
Sebastian Fellner <fellnerseb@gmail.com>
2021-03-01 17:32:37 +0100

    different version

--- static/custom-sw.js
+++ static/custom-sw.js
@@ -1,21 +1,44 @@
-console.log("Custom service worker!");
+console.alert("Custom service worker!");
+// window.$workbox.then((wb) => {
+//   console.log("got workbox", wb);
+//   wb.addEventListener("fetch", (event) => {
+//     if (event.request.method !== "POST") {
+//       event.respondWith(fetch(event.request));
+//       return;
+//     }
+//
+//     event.respondWith(
+//       (async () => {
+//         const formData = await event.request.formData();
+//         console.log("got post request");
+//         console.log("data is: ", formData);
+//         const link = formData.get("files") || "";
+//         console.log("files", link);
+//         return new Response("got your message");
+//       })()
+//     );
+//   });
+// });
+
+//service-worker.js:
 window.$workbox.then((wb) => {
-  console.log("got workbox", wb);
   wb.addEventListener("fetch", (event) => {
-    if (event.request.method !== "POST") {
-      event.respondWith(fetch(event.request));
+    const url = new URL(event.request.url);
+    if (
+      event.request.method === "POST" &&
+      url.pathname === "/" &&
+      url.searchParams.has("share-target")
+    ) {
+      event.respondWith(Response.redirect("/?receiving-file-share=1"));
+      event.waitUntil(
+        (async function () {
+          const client = await self.clients.get(event.resultingClientId);
+          const data = await event.request.formData();
+          const files = data.get("file");
+          client.postMessage({ files });
+        })()
+      );
       return;
     }
-
-    event.respondWith(
-      (async () => {
-        const formData = await event.request.formData();
-        console.log("got post request");
-        console.log("data is: ", formData);
-        const link = formData.get("files") || "";
-        console.log("files", link);
-        return new Response("got your message");
-      })()
-    );
   });
 });
