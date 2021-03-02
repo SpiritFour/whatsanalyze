588d2cedf41426d52ccd7612899a7d9a58f3d424
Moritz Wolf <code@moritzwolf.com>
2021-03-02 14:34:30 +0100

    add

--- components/Chat.vue
+++ components/Chat.vue
@@ -82,6 +82,18 @@ export default {
   },
   props: ["chat_"],
   methods: {
+    validURL(str) {
+      var pattern = new RegExp(
+        "^(https?:\\/\\/)?" + // protocol
+          "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+          "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+          "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+          "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+          "(\\#[-a-z\\d_]*)?$",
+        "i"
+      ); // fragment locator
+      return !!pattern.test(str);
+    },
     changeEgoTo(name) {
       this.selectedEgo = name;
     },
@@ -139,6 +151,7 @@ export default {
   max-width: 70% !important;
   margin: auto !important;
   text-align: center;
+  word-wrap: break-word;
 }
 .system .message {
   text-align: center;
