5655bc5c92e8685c4c4a3c9274e4109842ca3782
Moritz Wolf <code@moritzwolf.com>
2021-03-03 10:39:43 +0100

    fix day

--- components/ChatVisualization/Chat.vue
+++ components/ChatVisualization/Chat.vue
@@ -132,7 +132,7 @@ export default {
         "December",
       ];
       if (date) {
-        const day = date.getDay() + 1;
+        const day = date.getDate();
         const month = months[date.getMonth()];
         const year = date.getFullYear();
         const hour = date.getHours();
