ac6338e08f1b8df08e880987ac542ceff87645f5
Sebastian Fellner <fellnerseb@gmail.com>
2021-03-03 09:56:30 +0100

    now with sw from different app

--- static/custom-sw.js
+++ static/custom-sw.js
@@ -1,24 +1,3 @@
-// window.$workbox.then((wb) => {
-//   console.log("got workbox", wb);
-//   wb.addEventListener("fetch", (event) => {
-//     if (event.request.method !== "POST") {
-//       event.respondWith(fetch(event.request));
-//       return;
-//     }
-//
-//     event.respondWith(
-//       (async () => {
-//         const formData = await event.request.formData();
-//         console.log("got post request");
-//         console.log("data is: ", formData);
-//         const link = formData.get("files") || "";
-//         console.log("files", link);
-//         return new Response("got your message");
-//       })()
-//     );
-//   });
-// });
-
 console.log("Custom service worker!");
 // maybe i need servvice woker windows communicating with each other
 //service-worker.js:
@@ -28,24 +7,6 @@ self.addEventListener("message", function (e) {
   console.log(e);
 });
 
-// self.workbox.addEventListener("installed", (event) => {
-//   // If we don't do this we'll be displaying the notification after the initial installation, which isn't perferred.
-//   console.log("installed", event);
-// });
-//
-// self.workbox.addEventListener("message", (event) => {
-//   // If we don't do this we'll be displaying the notification after the initial installation, which isn't perferred.
-//   console.log("message", event);
-// });
-//window is here not defined
-// window.$workbox.then((w) => {
-//   console.log("workbox here:", JSON.stringify(w));
-//   w.addEventListener("message", (event) => {
-//     // If we don't do this we'll be displaying the notification after the initial installation, which isn't perferred.
-//     console.log("got message", event);
-//   });
-// });
-
 console.log("self: ", self);
 console.log("self: ", self.onpush);
 
@@ -53,53 +14,100 @@ self.onpush = (x) => {
   console.log(x);
 };
 
-self.onfetch = (event) => {
+// self.onfetch = (event) => {
+//   const url = new URL(event.request.url);
+//
+//   if (
+//     (event.request.method === "POST" &&
+//       url.pathname === "/" &&
+//       url.searchParams.has("share-target")) ||
+//     url.pathname === "/data2"
+//   ) {
+//     console.log(
+//       event,
+//       url,
+//       event.request.method,
+//       url.pathname,
+//       url.searchParams.has("share-target"),
+//       event.clientId
+//     );
+//     // we should only log this for better readability
+//     event.respondWith(Response.redirect("/?receiving-file-share=1"));
+//     event.waitUntil(
+//       (async function () {
+//         const client = await self.clients.get(
+//           event.clientId || event.resultingClientId
+//         );
+//         console.log("client in wait until", client);
+//         const data = await event.request.formData();
+//         console.log("data in wait until", data);
+//         data.forEach((b, c) => {
+//           console.log(b, c);
+//         });
+//         const files = data.get("file");
+//         console.log("files in wait until", files); //todo this is null, find a way to log data with all keys (FormDat)
+//         client.postMessage({ files });
+//       })()
+//     );
+//     return;
+//   }
+// };
+
+self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
 
+  // Don't care about other-origin URLs
+  if (url.origin !== location.origin) return;
+
   if (
-    (event.request.method === "POST" &&
-      url.pathname === "/" &&
-      url.searchParams.has("share-target")) ||
+    (url.pathname === "/" &&
+      url.searchParams.has("share-target") &&
+      event.request.method === "POST") ||
     url.pathname === "/data2"
   ) {
-    console.log(
-      event,
-      url,
-      event.request.method,
-      url.pathname,
-      url.searchParams.has("share-target"),
-      event.clientId
-    );
-    // we should only log this for better readability
-    event.respondWith(Response.redirect("/?receiving-file-share=1"));
-    event.waitUntil(
-      (async function () {
-        const client = await self.clients.get(
-          event.clientId || event.resultingClientId
-        );
-        console.log("client in wait until", client);
-        const data = await event.request.formData();
-        console.log("data in wait until", data);
-        data.forEach((b, c) => {
-          console.log(b, c);
-        });
-        const files = data.get("file");
-        console.log("files in wait until", files); //todo this is null, find a way to log data with all keys (FormDat)
-        client.postMessage({ files });
-      })()
-    );
+    serveShareTarget(event);
     return;
   }
-};
+});
 
-// if ("serviceWorker" in self) {
-//   console.log("service workger in navigation!!! yuhu");
-//   self.getRegistrations().then((registrations) => {
-//     console.log("all registrations:", registrations);
-//     for (const worker of registrations) {
-//       console.log("Service worker:", worker);
+export function serveShareTarget(event) {
+  const dataPromise = event.request.formData();
+
+  // Redirect so the user can refresh the page without resending data.
+  event.respondWith(Response.redirect("/?receiving-file-share=1"));
+
+  event.waitUntil(
+    (async function () {
+      // The page sends this message to tell the service worker it's ready to receive the file.
+      // await nextMessage("share-ready");
+      const client = await self.clients.get(
+        event.resultingClientId || event.clientId
+      );
+      console.log("client in wait until", client);
+      const data = await dataPromise;
+      console.log("data in wait until", data);
+      data.forEach((b, c) => {
+        console.log(b, c);
+      });
+      const file = data.get("file");
+      console.log("files in wait until", file);
+      client.postMessage({ file });
+    })()
+  );
+}
+
+// const nextMessageResolveMap = new Map();
+
+// /**
+//  * Wait on a message with a particular event.data value.
+//  *
+//  * @param dataVal The event.data value.
+//  */
+// function nextMessage(dataVal) {
+//   return new Promise((resolve) => {
+//     if (!nextMessageResolveMap.has(dataVal)) {
+//       nextMessageResolveMap.set(dataVal, []);
 //     }
+//     nextMessageResolveMap.get(dataVal).push(resolve);
 //   });
-// } else {
-//   console.log("no service workger in navigation");
 // }
