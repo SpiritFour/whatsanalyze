cee3e2f6032771a8117e118c34c318f0e236c63b
Moritz Wolf <code@moritzwolf.com>
2021-03-08 18:38:19 +0100

    emit ego to parent

--- components/ChatVisualization/Chat.vue
+++ components/ChatVisualization/Chat.vue
@@ -139,6 +139,7 @@ export default {
       return htmlMessage;
     },
     changeEgoTo(name) {
+      this.$emit("setEgo", name);
       this.selectedEgo = name;
     },
     _getDateString(date) {
