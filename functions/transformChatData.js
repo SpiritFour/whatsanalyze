62cc5b9f870e2411f523d260a6aa9b90f161ee93
Paul Kehnel <ga65coy@mytum.de>
2021-03-09 13:50:13 +0100

    Testing pushing to repo

--- functions/transformChatData.js
+++ functions/transformChatData.js
@@ -372,7 +372,7 @@ export class Chat {
           label: "Everyone",
           borderWidth: 1,
           lineTension: 0,
-          pointRadius: 1,
+          pointRadius: 0,
           pointHitRadius: 2,
           backgroundColor: hexToRgbA(chatColors[0]),
           borderColor: hexToRgbA("#0e6b0e", [1]),
