0d49e58a72f52d971135568adda69eccca5a95ce
Sebastian Fellner <fellnerseb@gmail.com>
2021-03-11 19:18:16 +0100

    disable analytics in dev

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -1,4 +1,10 @@
 import Vue from "vue";
 import VueGtag from "vue-gtag";
 
-Vue.use(VueGtag, { config: { id: "G-XYC2EWGZZ3" } });
+Vue.use(VueGtag, {
+  config: {
+    id: "G-XYC2EWGZZ3",
+    // eslint-disable-next-line no-undef
+    enabled: process.env.NUXT_ENV_LOCAL === undefined,
+  },
+});
