03aec9c5efec87594e1b4d3dcbbf4eff50f4f8e2
Paul Kehnel <ga65coy@mytum.de>
2021-03-12 19:17:54 +0100

    refactor getDateString. adding (dddd) will print the weekday name as well. (Suggestions)

--- functions/utils.js
+++ functions/utils.js
@@ -1,3 +1,4 @@
+import * as moment from "moment";
 export function downloadBase64File(contentBase64, fileName) {
   const downloadLink = document.createElement("a");
   document.body.appendChild(downloadLink);
@@ -9,38 +10,13 @@ export function downloadBase64File(contentBase64, fileName) {
   document.body.removeChild(downloadLink);
 }
 
-const zeroPad = (num, places) => String(num).padStart(places, "0");
-
 export function getDateString(date, includeTime = true) {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
   if (date) {
-    const day = date.getDate();
-    const month = months[date.getMonth()];
-    const year = date.getFullYear();
-    const hour = date.getHours();
-    const min = date.getMinutes();
-
-    return (
-      day +
-      " " +
-      month +
-      " " +
-      year +
-      (includeTime ? ", " + zeroPad(hour, 2) + ":" + zeroPad(min, 2) : "")
-    );
+    if (includeTime === true) {
+      return moment(date).format("MMMM Do YYYY h:mm");
+    } else {
+      return moment(date).format("dddd, MMMM Do YYYY");
+    }
   }
   return "";
 }
