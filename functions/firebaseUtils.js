7e4e01b131a6a55d7f644504f2c8087b72f5328b
Sebastian Fellner <fellnerseb@gmail.com>
2021-03-15 08:31:53 +0100

    easier token generation

--- functions/firebaseUtils.js
+++ functions/firebaseUtils.js
@@ -1,25 +1,20 @@
 export class Firebase {
   static getToken(eMail, numberToken = 10) {
-    const messageRef = window.$nuxt.$fire.firestore
-      .collection("token")
-      .doc(eMail);
-    let token = Array(numberToken)
-      .fill()
-      .map(() => Math.random().toString(36).substring(7));
-    let firebaseUpdate = {
-      tokens: window.$nuxt.$fireModule.firestore.FieldValue.arrayUnion(
-        ...token
-      ),
-    };
-    return messageRef
-      .get()
-      .then((a) => {
-        if (!a.exists) {
-          return messageRef.set(firebaseUpdate);
-        } else {
-          return messageRef.update(firebaseUpdate);
-        }
-      })
-      .then(() => token);
+    let db = window.$nuxt.$fire.firestore;
+
+    function addOneToken(eMail) {
+      return db
+        .collection("token")
+        .add({ eMail: eMail })
+        .then((messageRef) => {
+          return messageRef.id;
+        });
+    }
+
+    return Promise.all(
+      Array(numberToken)
+        .fill()
+        .map(() => addOneToken(eMail))
+    );
   }
 }
