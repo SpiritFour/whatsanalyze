60bbde16cfed688bb413a72b5653e99955c02ca5
mowolf <32100482+mowolf@users.noreply.github.com>
2021-03-17 15:45:38 +0000

    Deploying to gh-pages from @ SpiritFour/whatsanalyze@5613eac87c55e574d50f8e2eba5b52911b6a06da 🚀

--- _nuxt/static/1615995138/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615995138",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e39fd65d"}}}}(null,"\u002F"));
--- _nuxt/static/1615995138/switch-from-whatsapp-to-signal/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615995138",layout:"default",error:a,serverRendered:true,routePath:"\u002Fswitch-from-whatsapp-to-signal",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e39fd65d"}}}}(null));
