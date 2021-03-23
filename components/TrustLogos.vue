8fbe0aef3005c8154c8eff7c4598049d0e115b39
Paul Kehnel <ga65coy@mytum.de>
2021-03-23 20:22:11 +0100

    starting with blockquotes

--- components/TrustLogos.vue
+++ components/TrustLogos.vue
@@ -4,16 +4,21 @@
       ><div class="ml-5 mb-1 text-h5 font-weight-bold">Known from:</div></v-row
     >
     <v-row justify="center">
-      <v-col v-for="site in trustedSites" :key="site.description">
+      <v-col v-for="site in trustedSites" :key="site.name">
         <v-row justify="center">
-          <a :href="site.description">
+          <a :href="site.href">
             <v-avatar size="70"><img :src="site.image" /></v-avatar>
           </a>
         </v-row>
         <v-row class="pt-2" v-if="$vuetify.breakpoint.mdAndUp">
-          <div class="text-body-2">
-            {{ site.description }}
-          </div>
+          <figure>
+            <blockquote :cite="site.href">
+              <p>
+                {{ site.description }}
+              </p>
+            </blockquote>
+            <figcaption>—{{ site.name }}</figcaption>
+          </figure>
         </v-row>
       </v-col>
     </v-row>
@@ -27,25 +32,29 @@ export default {
     return {
       trustedSites: [
         {
+          name: "Chip.de",
           // eslint-disable-next-line no-undef
           image: require("~/assets/img/trust-logos/chip_logo.png"),
           href:
             "https://www.chip.de/downloads/webapp-WhatsAnalyze-WhatsApp-analysieren_183369368.html",
-          description: '"Fun-Facts und spannende Visualisierungen"',
+          description: "Fun-Facts und spannende Visualisierungen",
         },
         {
+          name: "Giga.de",
+
           // eslint-disable-next-line no-undef
           image: require("~/assets/img/trust-logos/giga_logo.png"),
           href:
             "https://www.giga.de/news/ueberraschende-einblicke-whatsapp-chats-kostenlos-analysieren/",
-          description: '"Die Analyse findet nur in eurem Browser statt"',
+          description: "Die Analyse findet nur in eurem Browser statt",
         },
         {
+          name: "Netzwelt.de",
           // eslint-disable-next-line no-undef
           image: require("~/assets/img/trust-logos/netzwelt.png"),
           href:
             "https://www.netzwelt.de/news/187295-whatsapp-webseite-analysiert-chats.html",
-          description: '"Zu welcher Uhrzeit ihr am häufigsten schreibt"',
+          description: "Zu welcher Uhrzeit ihr am häufigsten schreibt",
         },
       ],
     };
@@ -54,3 +63,18 @@ export default {
 </script>
 
 <style scoped></style>
+<style>
+blockquote {
+  margin: 0;
+}
+
+blockquote p {
+  padding: 15px;
+  background: #eee;
+  border-radius: 5px;
+}
+
+blockquote p::before {
+  content: "\201C";
+}
+</style>
