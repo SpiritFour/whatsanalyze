418427281298fd3432101d496eda874429235c72
Moritz Wolf <code@moritzwolf.com>
2021-04-09 15:10:25 +0200

    add lang switcher

--- components/GlobalHeader.vue
+++ components/GlobalHeader.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="nav pl-xs-8 pl-0 py-0 py-md-8 additional-height">
-    <v-container>
-      <nuxt-link to="/">
-        <h1 style="color: black; font-size: 2em" class="pl-md-16">
-          <img
-            src="~/assets/whatsanalyze-logo-black.png"
-            height="40px"
-            alt="chat icon"
-            style="vertical-align: sub"
-            class="pr-3"
-          />
-          WhatsAnalyze
-        </h1>
-      </nuxt-link>
+    <v-container class="pl-md-16">
+      <v-card flat tile color="#21a68d">
+        <v-card-actions>
+          <nuxt-link to="/">
+            <h1 style="color: black; font-size: 2em">
+              <img
+                src="~/assets/whatsanalyze-logo-black.png"
+                height="40px"
+                alt="WhatsAnalyze Logo"
+                style="vertical-align: sub"
+                class="pr-3"
+              />
+              WhatsAnalyze
+            </h1>
+          </nuxt-link>
+
+          <v-spacer></v-spacer>
+          <LanguageSwitcher />
+        </v-card-actions>
+      </v-card>
     </v-container>
   </div>
 </template>
 <script>
+import LanguageSwitcher from "./LanguageSwitcher.vue";
 export default {
+  components: { LanguageSwitcher },
   name: "GlobalHeader",
 };
 </script>
---
+++ components/LanguageSwitcher.vue
@@ -0,0 +1,40 @@
+<template>
+  <select v-model="selectedLocale">
+    <option
+      v-for="locale in availableLocales"
+      :key="locale.code"
+      :value="locale.code"
+    >
+      {{ locale.flag }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedLocale: this.$i18n.locale,
+      availableLocales: [
+        {
+          code: "en",
+          name: "English",
+          flag: "🇬🇧",
+          iso: "en-GB",
+        },
+        {
+          code: "de",
+          name: "Deutsch",
+          flag: "🇩🇪",
+          iso: "de-DE",
+        },
+      ],
+    };
+  },
+  watch: {
+    selectedLocale(newLocale) {
+      this.$router.push(this.switchLocalePath(newLocale));
+    },
+  },
+};
+</script>
