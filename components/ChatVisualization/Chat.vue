7f7571a35223a9f186e444d25ae890153a61a92e
Sebastian Fellner <fellnerseb@gmail.com>
2021-07-24 20:24:48 +0200

    urls doe not need to start and end with lineendings

--- components/ChatVisualization/Chat.vue
+++ components/ChatVisualization/Chat.vue
@@ -112,12 +112,12 @@ export default {
   methods: {
     parseMessage(message) {
       const validUrl = new RegExp(
-        "^(https?:\\/\\/)?" + // protocol
+        "(https?:\\/\\/)?" + // protocol
           "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
           "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
           "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
           "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-          "(\\#[-a-z\\d_]*)?$",
+          "(\\#[-a-z\\d_]*)?",
         "i"
       );
       const words = message.split(" ");
