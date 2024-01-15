6db6cb3b65d1daca91c8ab84a258f0f3fa22a5d1
athi <athi@netlight.com>
2024-01-15 17:24:33 +0100

    work in progress

---
+++ components/charts/EmojiCloud.vue
@@ -0,0 +1,80 @@
+<template>
+  <div ref="chartdiv"></div>
+</template>
+
+<script>
+import { Chat } from "~/functions/transformChatData";
+import { onlyEmoji } from "emoji-aware";
+
+export default {
+  name: "EmojiCloud",
+  props: {
+    chartdata: new Chat(),
+    minWordLength: {
+      type: Number,
+      default: 3,
+    },
+    minFontSize: {
+      type: Number,
+      default: 6,
+    },
+    randomness: {
+      type: Number,
+      default: 0.1,
+    },
+  },
+  data() {
+    return {
+      chart: null,
+      series: null,
+    };
+  },
+  methods: {
+    updateGraph() {
+      this.chartdata.getAllWords().then((words) => {
+        // Filter the words to include only emojis
+        const emojiData = words.filter((wordObj) => {
+          // Check if the word is an emoji
+          return onlyEmoji(wordObj.word).length > 0;
+        });
+        this.series.data = emojiData;
+      });
+    },
+    watch: {
+      chartdata: {
+        handler() {
+          this.updateGraph();
+        },
+        deep: true,
+      },
+    },
+    mounted() {
+      let {
+        am4core,
+        am4themes_animated,
+        am4plugins_wordCloud,
+      } = this.$am4core();
+      am4core.useTheme(am4themes_animated);
+      am4core.options.onlyShowOnViewport = true;
+
+      this.chart = am4core.create(
+        this.$refs.chartdiv,
+        am4plugins_wordCloud.WordCloud
+      );
+      this.series = this.chart.series.push(
+        new am4plugins_wordCloud.WordCloudSeries()
+      );
+      this.series.dataFields.word = "word";
+      this.series.dataFields.value = "freq";
+      this.series.labels.template.tooltipText = "[bold]{freq}[/] x {word}";
+      this.series.accuracy = 5;
+      this.updateGraph();
+    },
+    beforeDestroy: function () {
+      this.chart.dispose();
+    },
+  },
+};
+</script>
+
+<style scoped></style>
